e74aa4f2a15dfe62c2d30ff98670730cfa96e8b2
root <root@kndl.at>
2026-02-20 09:31:41 +0000

    Add Neuromod Island BipedalWalker experiment

--- worker/run_all.py
+++ worker/run_all.py
@@ -23,7 +23,7 @@ from pathlib import Path
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
 from experiments import cma_es, openai_es, hybrid_ff, curriculum, indirect
-from experiments import bipedal_cma, bipedal_es, bipedal_pbt, bipedal_hardcore, scaling, neuromod, island_model, island_advanced, neuromod_island, gpu_cma, gpu_bipedal
+from experiments import bipedal_cma, bipedal_es, bipedal_pbt, bipedal_hardcore, scaling, neuromod, island_model, island_advanced, neuromod_island, gpu_cma, gpu_bipedal, neuromod_island_bipedal
 
 
 METHODS = {
@@ -46,6 +46,7 @@ METHODS = {
     "neuromod_island": ("Neuromod Island Model", neuromod_island),
     "gpu_cma": ("GPU CMA-ES", gpu_cma),
     "gpu_bipedal": ("GPU BipedalWalker", gpu_bipedal),
+    "neuromod_island_bipedal": ("Neuromod Island BipedalWalker", neuromod_island_bipedal),
 }
 
 
